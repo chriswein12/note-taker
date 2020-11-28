c512f2dd77fa29019b8a1b77f1d6b85181c2a920
Chris Wein <chriswein12@yahoo.com>
2020-11-28 12:40:01 -0600

    Added code to the apiRoutes js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,16 @@
 const router = require('express').Router();
+const handler = require('../public/assets/js/index')
 const { notes } = require('../db/db.json');
 const { selectedNote } = require("../lib/notes.js")
 
 router.get('/notes', (req, res) => {
     let results = notes;
-    if (req.query) {
+    handler.getNotes(results);
          
-    }
 });
 
 router.post('/notes', (req, res) => {
+    handler.saveNote(req.body)
 
 });
 
